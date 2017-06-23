111a2feb31c0da8df7fcb0c97db9ad1499314b8f
EdimusPrime <edimusprime@Edimus-Prime.local>
2017-06-23 16:45:55 -0400

    Continued JS
    Some progress on JS. Having issues retrieving weather data at the
    moment.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 // API key for rest of code
 
-var API_KEY = "eb7a60b05dc5c676d33e0f9d5b383737";
+	var API_KEY = "eb7a60b05dc5c676d33e0f9d5b383737";
 
 
 $(function(){
@@ -14,23 +14,22 @@ var loc;
 
 $.getJSON('https://ipinfo.io', function(d){
   console.log(d)
-
   loc = d.loc.split(',');
   console.log(loc);
 
-  $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' 
-  	+ loc[0] + '&lon=' + loc[1] + '&APPID='+ API_KEY, function(wd){
-  	console.log('get the yayo ', + wd);
+  $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ loc[0] + '&lon=' + loc[1] + '&APPID='+ API_KEY, function(weather){
+  	console.log('get the yayo ', + weather);
 
-  	
-  });
+
+  })
 
   // console.log(loc[0]+' and '+ loc[1]);
 
 
 
  // checks user location and adds to page with city and region/state
-$('#location').text(d.city + ', '+ d.region);
+
+// $('#location').text(d.city + ', '+ d.region);
 
 })
 
